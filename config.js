45c814c76c8dd17979470c262fd139bba1eca0fb
ruchith527 <82458071+ruchith527@users.noreply.github.com>
2021-10-10 13:48:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 import firebase from 'firebase'
 require("@firebase/firestore")
 
-const firebaseConfig = {
- //Add configuration here
- 
-};
+var firebaseConfig = { apiKey: "AIzaSyA3k_WiRGTJK5t3O4mEP8VOERtxKqmLSQ0",
+                        authDomain: "chotu-868c1.firebaseapp.com",
+                        databaseURL:"http://chotu-868c1.firebaseio.com"
+                        projectId: "chotu-868c1", 
+                        storageBucket: "chotu-868c1.appspot.com",
+                        messagingSenderId: "982811372065",
+                        appId: "1:982811372065:web:48f05c2bc6b9d7020b1160" };
   firebase.initializeApp(firebaseConfig);
 
   export default firebase.firestore()
